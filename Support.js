81a6f3c37bbdef3027ed6b8bd2967a73ad096eac
Moni <monica.hernandez@stu.bmcc.cuny.edu>
2018-11-26 19:45:07 -0500

    adds support page

---
+++ Support.js
@@ -0,0 +1,10 @@
+<script type= "text/javascript">
+
+<form id= "support" align="center" method="post">
+<input type="button" value="Submit" onclick="codeAddress()"/>
+
+function codeAddress() {
+    
+        }
+    });
+}
