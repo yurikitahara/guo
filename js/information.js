c41b98f5da6df7a001f41d6b2a01ce9d10c8ed28
KseniiaKrasilich <31446682+KseniiaKrasilich@users.noreply.github.com>
2018-11-28 21:03:04 -0500

    added info

--- js/information.js
+++ js/information.js
@@ -17,10 +17,24 @@ window.addEventListener('load', function() {
             document.getElementById('profile-bio').style.display = 'none';
             document.getElementById('display-bio').textContent = "Bio " + data.bio;
           }
+          if (data.phone) {
+            document.getElementById('profile-phone').style.display = 'none';
+            document.getElementById('display-phone').textContent = "Get"+ " "+"in"+" "+"touch"+ " "+data.phone;
+          }
+          if (data.location) {
+            document.getElementById('profile-location').style.display = 'none';
+            document.getElementById('display-location').textContent = "My"+ " "+"location"+" "+data.location;
+          }
+          if (data.interest) {
+            document.getElementById('profile-interest').style.display = 'none';
+            document.getElementById('display-interest').textContent = "My"+ " "+"interests"+" "+ data.interest;
+          }
       });
     }
   });
-
+  const interestBtn = document.getElementById('interest-btn');
+  const locationBtn = document.getElementById('location-btn');
+  const phoneBtn = document.getElementById('phone-btn');
   const ageBtn = document.getElementById('age-btn');
   const bioBtn = document.getElementById('bio-btn');
   ageBtn.addEventListener('click', function(event) {
@@ -28,6 +42,15 @@ window.addEventListener('load', function() {
   });
   bioBtn.addEventListener('click', function(event) {
     updateInfo('bio');
+  });
+  phoneBtn.addEventListener('click', function(event) {
+    updateInfo('phone');
+  });
+  locationBtn.addEventListener('click', function(event) {
+    updateInfo('location');
+  });
+  interestBtn.addEventListener('click', function(event) {
+    updateInfo('interest');
   });
 
   function updateInfo(info) {
@@ -48,15 +71,15 @@ window.addEventListener('load', function() {
     });
   }
 //just for age//
-  // function updateAge() {
-  //   const age = document.getElementById('age').value;
+  // function updatePhone() {
+  //   const phone = document.getElementById('phone').value;
   //   const uid = firebase.auth().currentUser.uid;
   //   const db = firebase.database();
   //   const ref = db.ref('users').child(uid);
-  //   const promise = ref.update({ "age": age });
+  //   const promise = ref.update({ "phone": phone });
   //   promise.then(function() {
-  //     document.getElementById('profile-age').style.display = 'none';
-  //     document.getElementById('display-age').textContent = "Age " + age;
+  //     document.getElementById('profile-phone').style.display = 'none';
+  //     document.getElementById('display-phone').textContent = "Age " + phone;
   //   });
   //   promise.catch(function(error) {
   //     console.log(error);
