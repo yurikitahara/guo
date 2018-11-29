5682e1e1803018628104e6c0af3ac719fe5563a6
Moni <monica.hernandez@stu.bmcc.cuny.edu>
2018-11-28 20:48:01 -0500

    supportupdate

--- js/Support.js
+++ js/Support.js
@@ -1,16 +1,24 @@
-const postInput = document.getElementById('send');
+window.addEventListener('load', function () {
 
 
-function publishPost() {
-	const uid = firebase.auth().currentUser.uid;
-	const db = firebase.database();
-	const ref = db.ref('send').child(uid);
-	const postInfo = {
-		text: postInput.value,
-		date: Date.now(),
-		author: firebase.auth().currentUser.displayName,
-		id: uid
-	};
-	ref.push(postInfo);
-	postInput.value = "Thank you!";
-}
+    const sendBtn = document.getElementById('send');
+    const supportInput = document.getElementById('support-question');
+    sendBtn.addEventListener('click', submitFeedback);
+    
+    function submitFeedback() {
+        const uid = firebase.auth().currentUser.uid;
+        const db = firebase.database();
+        const ref = db.ref('support');
+        const info = {
+            text: supportInput.value,
+            date: Date.now(),
+            author: firebase.auth().currentUser.displayName,
+            id: uid
+        };
+        const promise = ref.push(info);
+        promise.then(function(success){
+            document.getElementById('greeting').style.display="block";
+        });
+    }
+                     
+});
