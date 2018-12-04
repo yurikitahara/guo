b9a1a8fae7056d94f53936aec95dbf98cdd474b3
Owen Roberts <oroberts@bmcc.cuny.edu>
2018-12-03 21:04:05 -0500

    new fixes

--- js/profile.js
+++ js/profile.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 window.addEventListener('load', function() {
 
 	/* adding an image */
@@ -33,46 +32,4 @@ window.addEventListener('load', function() {
 		}
 	});
 
-
-
-
-
-
-
-
-
-
-
-
-
 });
-=======
-
-
-
-// Get the modal
-var modal = document.getElementById('myModal');
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
->>>>>>> 72b83664892dfdb91f6b49334b12a03a7ff4af44
