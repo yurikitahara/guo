f1fa331e32fb81bd9a69d17276cd75bf38db1508
Moni <monica.hernandez@stu.bmcc.cuny.edu>
2018-12-10 21:15:33 -0500

    event location

--- event.js
+++ event.js
@@ -1,92 +1,92 @@
 function initMap() {
     const db = firebase.database();
-    const ref = db.ref('events');
+    const ref = db.ref('locations');
 
-    var bmccLocation = {
-        lat: 40.718802437879894,
-        lng: -74.01154518127441
-    };
-    
-
-   var contentString = '<div id="content">'+
-            '<div id="siteNotice">'+
-            '</div>'+
-            '<h1 id="firstHeading" class="firstHeading">Jacky Chan</h1>'+
-            '<div id="bodyContent">'+
-            '<p><b>Jacky Chan</b>, is looking for yoga coach</b></p>'+
-            '<p><a href="#">Please visit my profile for more information </p></a>'
-            '</div>'+
-            '</div>';
-
-        var infowindow = new google.maps.InfoWindow({
-          content: contentString
-        });
-
-        var marker = new google.maps.Marker({
-          // position: uluru,
-          map: map,
-          title: 'Uluru (Ayers Rock)'
-        });
-        
-
-
+    /* load events from the database */
+    const eventRef = db.ref('events');
 
     var map = new google.maps.Map(
-        document.getElementById('map'), 
-        {
-            center: bmccLocation,
+        document.getElementById('map'), {
+            center: {
+                lat: 40.718802437879894,
+                lng: -74.01154518127441
+            },
             zoom: 10
         }
     );
-    
-    ref.on('child_added', function(snapshot) {
+
+    eventRef.on('child_added', function (snapshot) {
         const event = snapshot.val();
-        var marker = new google.maps.Marker({
-            position: event.position,
-            title: event.title,
-            map: map
+        const url = 'event.html?id=' + snapshot.key;
+        console.log(event);
+        
+        console.log( '<a href="' + url + '">Click here</a>');
+
+        const locationRef = db.ref('locations').child(event.location);
+        const locPromise = locationRef.once('value');
+        locPromise.then(function (response) {
+            if (response) {
+                const location = response.val();
+                console.log(location);
+
+                var contentString = 
+                    '<div id="content">' +
+                    '<div id="siteNotice">' +
+                    '</div>' +
+                    '<h1 id="firstHeading" class="firstHeading">'+
+                    event.date + event.description 
+                '</h1>' +
+                    '<div id="bodyContent">' +
+                    '<p><b>User</b>, is looking new event</b></p>' +
+                    '<p><a href="' + url +'">' +event.description +
+                '</p></a>' +
+                '</div>' +
+                '</div>';
+
+                var infowindow = new google.maps.InfoWindow({
+                    content: contentString
+                });
+
+                var marker = new google.maps.Marker({
+                    position: location.position,
+                    title: location.title,
+                    map: map
+                });
+
+                marker.addListener('click', function () {
+                    map.setZoom(10);
+                    map.setCenter(marker.getPosition());
+                    infowindow.open(map, marker);
+                });
+            }
 
         });
-
-        marker.addListener('click', function() {
-            map.setZoom(10);
-            map.setCenter(marker.getPosition());
-            infowindow.open(map, marker);
+        locPromise.catch(function (error) {
+            console.log(error);
         });
+
+
+
+
     });
 
-    
-    
-    
-    
-    map.addListener('click', function(event) {
+
+
+
+
+    map.addListener('click', function (event) {
         console.log(event.latLng.lat());
         console.log(event.latLng.lng());
-        
 
-        
+
+
         const postInfo = {
             position: {
                 lat: event.latLng.lat(),
                 lng: event.latLng.lng()
             },
-            title: "New Location"
-         };
+            title: "Event Location"
+        };
         ref.push(postInfo);
     });
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
