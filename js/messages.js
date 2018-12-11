7eab2374f310eb52515c02d572852df719f7e071
LloydCss <33126293+LloydCss@users.noreply.github.com>
2018-12-10 21:10:38 -0500

    Add files via upload

--- js/messages.js
+++ js/messages.js
@@ -1,55 +1,68 @@
-window.addEventListener('load', function() {
-	
-	// global variables
-	const postsDiv = document.getElementById('messages');
-	const postRef = firebase.database().ref('messages');
-	
-	postRef.on('child_added', function(snapshot) {
-		createPost(snapshot.key, snapshot.val());
-	});
-	
-	function createPost(key, post) {
-		
-		// post container
-		const postDiv = document.createElement('div');
-		postDiv.classList.add('messages');
-		
-		// post text
-		const postText = document.createElement('div');
-		postText.classList.add('post-text');
-		postText.textContent = post.text;
-		
-		// post info 
-		const postInfo = document.createElement('div');
-		postInfo.classList.add('post-info');
-		
-		const postAuthor = document.createElement('span');
-		postAuthor.classList.add('post-author');
-		postAuthor.textContent = post.author;
-		
-		const postDate = document.createElement('span');
-		postDate.classList.add('post-date');
-		postDate.textContent = new Date(post.date).toLocaleString('en-us', {month: 'long', year: 'numeric', weekday: 'long', day: 'numeric' });
-		
-		postInfo.innerHTML += "by ";
-		postInfo.appendChild(postAuthor);
-		postInfo.innerHTML += " on ";
-		postInfo.appendChild(postDate);
-		
-		postDiv.appendChild(postText);
-		postDiv.appendChild(postInfo);
-		
-		postsDiv.insertBefore(postDiv, postsDiv.firstElementChild);
-	}
+window.addEventListener('load', function () {
+    
+    // posting a message
+    const publishButton = document.getElementById('submit-message');
+const messageInput = document.getElementById('message-body');
+publishButton.addEventListener('click', publishMessage);
+    
+    function publishMessage() {
+	const uid = firebase.auth().currentUser.uid;
+	const db = firebase.database();
+	const ref = db.ref('messages');
+	const messageInfo = {
+		text: messageInput.value,
+		date: Date.now(),
+		author: firebase.auth().currentUser.displayName,
+		id: uid
+	};
+	ref.push(messageInfo);
+	messageInput.value = "";
+}
+
+    // global variables
+    const messageDiv = document.getElementById('message');
+    const messageRef = firebase.database().ref('message');
+
+    messageRef.on('child_added', function (snapshot) {
+        createMessage(snapshot.key, snapshot.val());
+    });
+
+    function createPost(key, message) {
+
+        // post container
+        const messageDiv = document.createElement('div');
+        messageDiv.classList.add('message');
+
+        // post text
+        const messageText = document.createElement('div');
+        messageText.classList.add('message-text');
+        messageText.textContent = post.text;
+
+        // post info 
+        const postInfo = document.createElement('div');
+        messageInfo.classList.add('message-info');
+
+        const postAuthor = document.createElement('span');
+        messageAuthor.classList.add('message-author');
+        messageAuthor.textContent = message.author;
+
+        const postDate = document.createElement('span');
+        messageDate.classList.add('message-date');
+        messageDate.textContent = new Date(message.date).toLocaleString('en-us', {
+            month: 'long',
+            year: 'numeric',
+            weekday: 'long',
+            day: 'numeric'
+        });
+
+        messageInfo.innerHTML += "by ";
+        messageInfo.appendChild(postAuthor);
+        messageInfo.innerHTML += " on ";
+        messageInfo.appendChild(postDate);
+
+        messageDiv.appendChild(postText);
+        messageDiv.appendChild(postInfo);
+
+        messageDiv.insertBefore(postDiv, postsDiv.firstElementChild);
+    }
 });
-
-
-
-
-
-
-
-
-
-
-
