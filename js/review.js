a78c20010d424dbe6c660650b3d234843564d658
KseniiaKrasilich <31446682+KseniiaKrasilich@users.noreply.github.com>
2018-12-13 12:53:59 -0500

    added comments

--- js/review.js
+++ js/review.js
@@ -1,58 +1,61 @@
-window.addEventListener('load', function() {
+window.addEventListener('load', function () {
+    
+    const reviewId = location.search.split('=')[1];
 
-  const postsDiv = document.getElementById('posts');
-	const postRef = firebase.database().ref('posts');
+    
+    firebase.auth().onAuthStateChanged(function(user) {
+		if (user) {
+            if (user.uid == reviewId) {
+                document.getElementById('write-review').style.display='none';
+            }
+        }
+    });
 
-  postRef.on('child_added', function(snapshot) {
-    createPost(snapshot.key, snapshot.val());
-  });
+    const userDiv = document.getElementById('user');
+    const reviewButton = document.getElementById('submit-review');
+    const reviewInput = document.getElementById('review-body');
 
-  function createPost(post) {
-  	const postDiv = document.createElement('div');
-  	postDiv.classList.add('post');
-  	const postText = document.createElement('div');
-  	postText.classList.add('post-text');
-  	const postInfo = document.createElement('div');
-  	postInfo.classList.add('post-info');
-
-  	const postAuthor = document.createElement('span');
-  	postAuthor.classList.add('post-author');
-  	const postDate = document.createElement('span');
-  	postDate.classList.add('post-date');
-
-  	postText.textContent = post.text;
-  	postAuthor.textContent = post.author;
-  	postDate.textContent = new Date(post.date).toLocaleString('en-us', { month: 'long', year: 'numeric', weekday: 'long', day: 'numeric' })
-
-  	postDiv.appendChild(postText);
-  	postDiv.appendChild(postInfo);
-  	postInfo.innerHTML += "by ";
-  	postInfo.appendChild(postAuthor);
-  	postInfo.innerHTML += " on ";
-  	postInfo.appendChild(postDate);
-
-  	/* chronological order */
-  	// postsDiv.appendChild(postDiv);
-  	/* reverse chronological order */
-  	postsDiv.insertBefore(postDiv, postsDiv.firstElementChild);
-  }
-
-  const publishButton = document.getElementById('submit-post');
-  const postInput = document.getElementById('post-body');
-
-  publishButton.addEventListener('click', publishPost(){
-    const uid = firebase.auth().currentUser.uid;
     const db = firebase.database();
-    const ref = db.ref('posts').child(uid);
-    const postInfo = {
-      text: postInput.value,
-      date: Date.now(),
-      author: firebase.auth().currentUser.displayName,
-      id: uid
-    };
-    ref.push(postInfo);
-    postInput.value = "";
-  });
+    const ref = db.ref('users').child(reviewId);
+    ref.once('value', function (user) {
+        const data = user.val();
+        console.log(data);
+        // console.log(data);
+
+        // display user name
+
+        const div = document.createElement('div');
+        div.textContent = "User: " + data.displayName;
+        userDiv.appendChild(div);
+        
+        for (const key in data.reviews) {
+            const review = data.reviews[key];
+            console.log(review);
+            const div = document.createElement('div');
+            div.textContent = "Review: " + review.text;
+            userDiv.appendChild(div);
+            
+            const author = document.createElement('a');
+            author.href = "reviews.html?id=" + review.authorId;
+            author.textContent = "by " + review.author;
+            userDiv.appendChild(author);
+        }
+
+    });
+
+    reviewButton.addEventListener('click', function () {
+        const uid = firebase.auth().currentUser.uid;
+        const db = firebase.database();
+        const ref = db.ref('users').child(reviewId).child('reviews');
+        const review = {
+            text: reviewInput.value,
+            date: Date.now(),
+            author: firebase.auth().currentUser.displayName,
+            authorId: uid
+        };
+        ref.push(review);
+        reviewInput.value = "";
+    });
 
 
 });
